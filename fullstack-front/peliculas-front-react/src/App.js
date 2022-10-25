2759ad797bbf4d1590389c3924c689e58a945d7e
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2022-10-25 13:01:55 -0300

    barra de navegación hecha

--- fullstack-front/peliculas-front-react/src/App.js
+++ fullstack-front/peliculas-front-react/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
---
+++ fullstack-front/peliculas-front-react/src/layout/Navbar.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div>
+
+            <nav className="navbar navbar-expand-lg bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">App Películas</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <Link className="btn btn-outline-light">Añadir película</Link>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
